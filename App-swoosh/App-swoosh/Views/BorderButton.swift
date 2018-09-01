d1ecdacbef6fd95c80dc7d113e81f46890b9c2b2
OnOApps <onoapps@OnOAppss-MacBook-Pro.local>
2018-09-01 12:44:59 +0300

    swift new pro

---
+++ App-swoosh/App-swoosh/Views/BorderButton.swift
@@ -0,0 +1,27 @@
+//
+//  BorderButton.swift
+//  App-swoosh
+//
+//  Created by OnOApps on 01/09/2018.
+//  Copyright © 2018 OnOApps. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.borderWidth = 2.0
+        layer.borderColor = UIColor.white.cgColor
+    }
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
